9e2a9d2861e45bad09ae9ba05b094e3aac8fbd87
Andikaim <andikagamers50@gmail.com>
2021-11-27 05:32:20 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('login/github','GithubController@redirectToProvider');
+Route::get('login/github/callback','GithubController@handleProviderCallback');
